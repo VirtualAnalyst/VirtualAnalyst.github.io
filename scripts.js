6254373dc23ee12d7dba2e7af1f2dd56acd91d73
John Doe <johndoe@example.com>
2023-09-19 14:52:36 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const listeMots    = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", 
+                      "Merci pour le poisson"]
+
+function afficherResultat(score, nbMotsProposes) {
+    // Récupération de la zone dans laquelle on va écrire le score
+    let spanScore = document.querySelector(".zoneScore span")
+    // Ecriture du texte
+    let affichageScore = `${score} / ${nbMotsProposes}` 
+    // On place le texte à l'intérieur du span. 
+    spanScore.innerText = affichageScore}
+
+function afficherProposition(proposition) {
+    let zoneProposition = document.querySelector(".zoneProposition")
+    zoneProposition.innerText = proposition}
+
+function lancerJeu() {
+    initAddEventListenerPopup()
+    let score = 0
+    let i = 0
+    let listeProposition = listeMots
+
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
+
+    afficherProposition(listeProposition[i])
+
+    // Gestion de l'événement click sur le bouton "valider"
+    btnValiderMot.addEventListener("click", () => {
+        if (inputEcriture.value === listeProposition[i]) {score++}
+        i++
+        afficherResultat(score, i)
+        inputEcriture.value = ''
+        if (listeProposition[i] === undefined) {
+            afficherProposition("Le jeu est fini")
+            btnValiderMot.disabled = true} 
+        else {afficherProposition(listeProposition[i])}})
+
+    // Gestion de l'événement change sur les boutons radios. 
+    let listeBtnRadio = document.querySelectorAll(".optionSource input")
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+            // Si premier liste des phrases
+            if (event.target.value === "1") {listeProposition = listeMots} 
+            else {listeProposition = listePhrases}
+            // Et on modifie l'affichage en direct. 
+            afficherProposition(listeProposition[i])})}
+
+    afficherResultat(score, i)}
+
+lancerJeu()
